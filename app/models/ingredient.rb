85056d3ed4073646c38c7393e7de0aebdd7b11d4
barbaradecares <barbaradecares@hotmail.com>
2018-11-06 12:45:04 -0600

    done

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,11 @@
 class Ingredient < ActiveRecord::Base
   belongs_to :recipe
+  class Song < ActiveRecord::Base
+    def ingredient_attributes=(ingredient)
+      self.ingredient = Ingredient.find_or_create_by(name: ingredient.name)
+      self.ingredient.update(ingredient)
+    end
+  end
+  
+  
 end
